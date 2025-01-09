d68f302da240324aa6221aa1b1b06e99ab5c3944
Aditya Choudhary <tejash835274@gmail.com>
2025-01-10 02:00:02 +0530

    da

--- backend/src/utils/Cloudinary.js
+++ backend/src/utils/Cloudinary.js
@@ -1,24 +1,22 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 import 'dotenv/config';
 import { error } from "console";
 
 const uploadOnCloudinary = async (localfilepath) => {
     try {
-        cloudinary.config({ 
-            cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-            api_key: process.env.CLOUDINARY_API_KEY, 
-            api_secret: process.env.CLOUDINARY_API_SECRET 
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
         });
 
         if (!localfilepath) throw new Error("localfilepath is missing!");
 
         const response = await cloudinary.uploader.upload(localfilepath, {
-            transformation: [
-              { quality: 'auto' }, // Auto-optimize quality
-              { fetch_format: 'auto' }, // Use modern formats like WebP if supported
-            ],
-          }).catch(error => {
+            quality: 'auto', // Auto-optimize quality
+            fetch_format: 'auto', // Use modern formats like WebP if supported
+        }).catch(error => {
             console.log(error);
             throw new Error("Unable to upload to cloudinary!");
         });
